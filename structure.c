d20edc88971cd12ee1ee542bfa065d624c1b6792
Theo Fornier <theo.fornier@edu.ece.fr>
2022-10-26 19:13:56 +0200

    struct

---
+++ structure.c
@@ -0,0 +1,5 @@
+//
+// Created by theof on 26/10/2022.
+//
+
+#include "structure.h"
---
+++ structure.h
@@ -0,0 +1,45 @@
+#ifndef ECE_CITY_3B_STRUCTURE_H
+#define ECE_CITY_3B_STRUCTURE_H
+
+
+typedef struct Liste {
+    struct Liste* suivant;
+    int numero;
+};
+
+typedef struct Route{
+    char symbole;
+    int pos_x;
+    int pos_y;
+    struct Liste adjacente_maison;
+    struct Liste adjacente_route;
+    struct Liste adjacente_batiment;
+};
+
+typedef struct Case{
+    int occupe;
+    char symbole;
+    int pos_x;
+    int pos_y;
+};
+
+typedef struct Batiment{
+    char symbole;
+    int capacite;
+    int pos_x;
+    int pos_y;
+    struct Liste routeAdjacente;
+};
+
+typedef struct Maison{
+    char symbole;
+    int niveau;
+    int eau;
+    int electricite;
+    struct Liste routeAdjacente;
+    int pos_x;
+    int pos_y;
+};
+
+
+#endif //ECE_CITY_3B_STRUCTURE_H
