72a7a96b7822f3bf1c81fe5593a719a80bd4bcdd
TheoFornier <92516131+TheoFornier@users.noreply.github.com>
2022-11-09 16:20:46 +0100

    Update affichage.c

--- affichage.c
+++ affichage.c
@@ -21,3 +21,42 @@ void afficherCarte(Case** map){
         printf("\n");
     }
 }
+
+
+void afficherCarteRaylib(){
+    // initialisation
+    const int screenWidth = 2000;
+    const int screenHeight = 1450;
+
+    InitWindow(screenWidth, screenHeight, "carte");
+    Texture2D texture = LoadTexture("info/texturepelouse.jpg");        // chargement texture
+
+    // camera en 3D
+    Camera3D camera = { 0 };
+    camera.position = (Vector3){ 10.0f, 10.0f, 10.0f };
+    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
+    SetCameraMode(camera, CAMERA_FREE); // mode de la camera
+
+    SetTargetFPS(60);                   // fps
+    while (!WindowShouldClose()){
+        // mise a jour
+        UpdateCamera(&camera);
+
+        // dessiner
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        BeginMode3D(camera);
+        DrawGrid(46, 2);
+
+
+        EndMode3D();
+        EndDrawing();
+    }
+
+    CloseWindow();
+}
